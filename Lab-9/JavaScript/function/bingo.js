b1d21c2ec05ac6fdea2557c751568450ef9c6a2d
zaheerahmadp <69234659+zaheerahmadp@users.noreply.github.com>
2021-09-12 19:33:01 +0530

    Add files via upload

---
+++ Lab-9/JavaScript/function/bingo.js
@@ -0,0 +1,22 @@
+window.onload = initAll;
+
+function initAll() {
+	if (document.getElementById) {
+		document.getElementById("pick").onclick = displayNumber;
+	}
+	else {
+		alert("Sorry, your browser doesn't support this script");
+	}
+}
+
+function displayNumber() {
+
+
+
+}
+
+function generateNumber() {
+
+
+
+}
